e46900f1a7679d13af7d9af4a02e2a99d3ad798c
Jan Zedník <84737140+Johny-Butcher@users.noreply.github.com>
2025-04-12 11:53:18 +0200

    dsa

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+  const ol = process.env.OL;
+  const al = process.env.AL;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -21,7 +23,11 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            {al}
+
+          </li><li className="tracking-[-.01em]">
+            {ol}
+
           </li>
         </ol>
 
